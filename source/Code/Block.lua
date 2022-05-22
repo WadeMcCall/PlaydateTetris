c7c33a17ddf456ded9442392ad54920808669c66
WadeMcCall <danceifuwantto@gmail.com>
2022-05-21 21:44:44 -0500

    fix errors

--- source/Code/Block.lua
+++ source/Code/Block.lua
@@ -25,7 +25,7 @@ end
 class("TBlock").extends("Block")
 
 function TBlock:init(_x, _y)
-    TBlock.super.init(_x, _y)
+    TBlock.super:init(_x, _y)
 end
 
 ----------
@@ -34,7 +34,7 @@ end
 class("IBlock").extends("Block")
 
 function IBlock:init(_x, _y)
-    IBlock.super.init(_x, _y)
+    IBlock.super:init(_x, _y)
 end
 
 ----------
@@ -43,7 +43,7 @@ end
 class("LBlock").extends("Block")
 
 function LBlock:init(_x, _y)
-    LBlock.super.init(_x, _y)
+    LBlock.super:init(_x, _y)
 end
 
 ------------------
@@ -52,7 +52,7 @@ end
 class("ReverseLBlock").extends("Block")
 
 function ReverseLBlock:init(_x, _y)
-    ReverseLBlock.super.init(_x, _y)
+    ReverseLBlock.super:init(_x, _y)
 end
 
 ----------
@@ -61,7 +61,7 @@ end
 class("SBlock").extends("Block")
 
 function SBlock:init(_x, _y)
-    SBlock.super.init(_x, _y)
+    SBlock.super:init(_x, _y)
 end
 
 ---------------
@@ -70,7 +70,7 @@ end
 class("SquareBlock").extends("Block")
 
 function TBlock:init(_x, _y)
-    TBlock.super.init(_x, _y)
+    TBlock.super:init(_x, _y)
 end
 
 ----------
@@ -79,5 +79,5 @@ end
 class("ZBlock").extends("Block")
 
 function TBlock:init(_x, _y)
-    TBlock.super.init(_x, _y)
+    TBlock.super:init(_x, _y)
 end
